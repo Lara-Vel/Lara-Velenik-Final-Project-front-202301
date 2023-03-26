3958d8b9d1fedc77ce3e9400f657c3d09cd33bc8
Lara-Vel <lara.vel@outlook.com>
2023-03-27 01:25:39 +0200

    deleted previus main layout

--- src/layout/MainLayout.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import MainLayout from './MainLayout';
-
-test('render page', () => {
-  render(<MainLayout />, { wrapper: MemoryRouter });
-  expect(true).toBe(true);
-});
