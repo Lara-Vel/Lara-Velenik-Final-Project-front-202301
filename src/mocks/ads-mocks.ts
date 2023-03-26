6fd05d2c1d17ab73167993d3a3cba8616b82d30c
Lara-Vel <lara.vel@outlook.com>
2023-03-27 01:33:45 +0200

    add ads mock,get and error handler,test utils and new reducers

---
+++ src/mocks/ads-mocks.ts
@@ -0,0 +1,15 @@
+export const adsFullFilledResponse = {
+  ads: [
+    {
+      name: 'Lara',
+      surname: 'Velenik',
+      breed: 'Oriental',
+      email: 'lara@gmail.com',
+      phone: '611111111',
+      city: 'Sevilla',
+      image: 'cat.webp',
+    },
+  ],
+  failed: false,
+  idle: true,
+};
