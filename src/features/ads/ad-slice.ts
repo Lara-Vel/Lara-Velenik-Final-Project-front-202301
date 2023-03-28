f511a979b3b6e6baaba67c801b4c0c2489a3aacd
Lara-Vel <lara.vel@outlook.com>
2023-03-28 11:11:32 +0200

    add post method to api and create add slice

--- src/features/ads/ad-slice.ts
+++ src/features/ads/ad-slice.ts
@@ -1,24 +1,35 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 import { Ad } from '../../models/ads-model';
-import { getAllAds } from './ad-api';
+import { createNewAd, getAllAds } from './ad-api';
 
 export type AdStatus = 'idle' | 'success' | 'error';
+
+export type NewAdStatus = 'idle' | 'success' | 'error' | 'notUsed';
 
 export interface AdsResponse {
   message: string;
   ads: Ad[];
 }
 
+export interface CreateAdResponse {
+  message: string;
+  ads: Ad;
+}
+
 export interface Adstate {
   ads: Ad[];
   status: AdStatus;
+  adStatus: NewAdStatus;
+  createAdStatus: NewAdStatus;
   responseMessage: string | undefined;
 }
 
 const initialState: Adstate = {
   ads: [],
   status: 'idle',
+  adStatus: 'notUsed',
+  createAdStatus: 'notUsed',
   responseMessage: '',
 };
 
@@ -31,6 +42,20 @@ export const getAllAdsAsync = createAsyncThunk('ads/getAllAds', async () => {
   }
   return data;
 });
+
+export const createAdAsync = createAsyncThunk(
+  `ads/createAd`,
+  async (newAdForm: HTMLFormElement) => {
+    const newAd = new FormData(newAdForm);
+    const apiResponse = await createNewAd(newAd);
+
+    return {
+      message: 'ok',
+      ads: { breed: '', city: '', image: '' },
+      apiResponse,
+    } as CreateAdResponse;
+  },
+);
 
 export const adsSlice = createSlice({
   name: 'adsSlice',
@@ -52,6 +77,25 @@ export const adsSlice = createSlice({
 
     builder.addCase(getAllAdsAsync.rejected, (state, action) => {
       state.status = 'error';
+      state.responseMessage = action.error.message;
+    });
+    builder.addCase(createAdAsync.pending, state => {
+      state.status = 'idle';
+      state.createAdStatus = 'idle';
+    });
+
+    builder.addCase(
+      createAdAsync.fulfilled,
+      (state, action: PayloadAction<CreateAdResponse>) => {
+        state.status = 'success';
+        state.createAdStatus = 'success';
+        state.responseMessage = action.payload.message;
+      },
+    );
+
+    builder.addCase(createAdAsync.rejected, (state, action) => {
+      state.status = 'error';
+      state.createAdStatus = 'error';
       state.responseMessage = action.error.message;
     });
   },
