56fd14502ddbecbde0a10b709cb6c34d120d93ce
Lara-Vel <lara.vel@outlook.com>
2023-03-29 13:23:48 +0200

    add more characterist to ad model

--- src/models/ads-model.ts
+++ src/models/ads-model.ts
@@ -1,16 +1,26 @@
 export interface Ad {
+  _id: string;
   breed: string;
   city: string;
   image: string;
 }
 
 export interface DetailAd {
+  _id: string;
+  breed: string;
   name: string;
   surname: string;
-  breed: string;
   email: string;
   phone: string;
   city: string;
   image: string;
   description: string;
 }
+
+export type AdId = Pick<DetailAd, '_id'>;
+
+export type ApiError = {
+  message: string;
+};
+
+export type ApiResponse<T> = ApiError | T;
