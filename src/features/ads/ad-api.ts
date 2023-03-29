50d08d5d9e4b418341693971d957a6c06dc1c88e
Lara-Vel <lara.vel@outlook.com>
2023-03-29 13:20:47 +0200

    add getAdById

--- src/features/ads/ad-api.ts
+++ src/features/ads/ad-api.ts
@@ -1,3 +1,5 @@
+import { ApiError, ApiResponse, DetailAd } from '../../models/ads-model';
+
 export const getAllAds = async () => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/ads`, {
     headers: {
@@ -27,4 +29,24 @@ export const createNewAd = async (newAd: FormData) => {
   }
 
   return response;
+};
+
+export const getAdById = async (id: string): Promise<ApiResponse<DetailAd>> => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+  );
+  const adResponse: ApiResponse<DetailAd> = await response.json();
+
+  if (!response.ok) {
+    throw new Error((adResponse as ApiError).message);
+  }
+
+  return adResponse as DetailAd;
 };
