17c72d5cc1123671c0ce69af7c49e68b35899011
Lara-Vel <lara.vel@outlook.com>
2023-03-29 13:26:32 +0200

    add new handlers and lines to test utils

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -27,10 +27,140 @@ export const handlers = [
       return res(ctx.status(200), ctx.json(adsFullFilledResponse));
     },
   ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f4`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f4',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Azul Ruso',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Azul Ruso',
+        }),
+      );
+    },
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f1`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f1',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Balinés',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Balinés',
+        }),
+      );
+    },
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f2`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f2',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Bengalí',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Bengalí',
+        }),
+      );
+    },
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f3`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f3',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Bosque de Noruega',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Bosque de Noruega',
+        }),
+      );
+    },
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f5`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f5',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Oriental',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Oriental',
+        }),
+      );
+    },
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/641db6f0d1fb673e861889f6`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: '641db6f0d1fb673e861889f6',
+          name: 'Lara',
+          surname: 'Velenik',
+          breed: 'Siberiano',
+          email: 'lara@gmail.com',
+          phone: '611111111',
+          city: 'Sevilla',
+          image: 'cat.webp',
+          description: 'Soy un gato Siberiano',
+        }),
+      );
+    },
+  ),
 ];
 
 export const errorHandlers = [
   rest.get(`${process.env.REACT_APP_API_URL}/api/v1/ads`, (_req, res, ctx) => {
     return res.once(ctx.status(500), ctx.json(null));
   }),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}/api/v1/ads/4321`,
+    (_req, res, ctx) => {
+      return res.once(
+        ctx.status(404),
+        ctx.json({ msg: 'El anuncio buscado no existe' }),
+      );
+    },
+  ),
 ];
