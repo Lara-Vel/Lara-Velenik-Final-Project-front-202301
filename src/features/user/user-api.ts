f03a51c0e6d5b45bef58fcb81e754cc829438fe2
Lara-Vel <lara.vel@outlook.com>
2023-05-25 02:09:30 +0200

    Add registerTokenByUser function to user-api.ts

--- src/features/user/user-api.ts
+++ src/features/user/user-api.ts
@@ -1,4 +1,8 @@
-import { UserCredentials, UserResponse } from '../../models/user-model';
+import {
+  NewUserCredentials,
+  UserCredentials,
+  UserResponse,
+} from '../../models/user-model';
 
 export const getTokenByUser = async (
   user: UserCredentials,
@@ -10,6 +14,27 @@ export const getTokenByUser = async (
     },
     body: JSON.stringify(user),
   });
+
+  if (!response.ok) {
+    throw new Error('Error datos incorrectos');
+  }
+
+  return response.json();
+};
+
+export const registerTokenByUser = async (
+  newUser: NewUserCredentials,
+): Promise<UserResponse> => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/auth/register`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    },
+  );
 
   if (!response.ok) {
     throw new Error('Error datos incorrectos');
