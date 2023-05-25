2705b62efeacf143b906be43a294cd957aefb491
Lara-Vel <lara.vel@outlook.com>
2023-05-25 02:05:43 +0200

    Add Register page with test

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '../../features/user/components/registerForm/RegisterForm';
+
+const Register = () => {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
