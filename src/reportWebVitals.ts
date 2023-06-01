a1714eb25fa80c56a587061ce686231f4ddc845b
Lara-Vel <lara.vel@outlook.com>
2023-06-01 02:17:49 +0200

    expected promise resolved

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,18 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = async (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    const webVitals = await import('web-vitals');
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
+    try {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    } catch (error: any) {
+      throw new Error('Error in reportWebVitals:' + error);
+    }
   }
 };
 
