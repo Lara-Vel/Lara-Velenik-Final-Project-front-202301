6e9024a645f16218d5c44d747029a4d2f6d085f0
Lara-Vel <lara.vel@outlook.com>
2023-06-01 03:32:06 +0200

    restore reportWebVitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,18 +1,14 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = async (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    const webVitals = await import('web-vitals');
-    const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
-    try {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    } catch (error: any) {
-      throw new Error('Error in reportWebVitals:' + error);
-    }
+    });
   }
 };
 
