20c667220fdf8d12b89cf5ebac10b4ca25bd6b2b
Lara-Vel <lara.vel@outlook.com>
2023-07-11 22:27:40 +0200

    change home page with new test,add accordion responsive

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { store } from '../../app/store';
 import Home from './Home';
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
 
 test('Given a Home page, when rendered then it should the home', () => {
   render(
@@ -12,6 +14,33 @@ test('Given a Home page, when rendered then it should the home', () => {
       </MemoryRouter>
     </Provider>,
   );
-  const heading = screen.getAllByRole('heading');
-  expect(heading.length).toBe(1);
+
+  const accordionHeaders = screen.getAllByRole('heading', { level: 2 });
+  const firstAccordionHeader = accordionHeaders[0];
+
+  expect(firstAccordionHeader.classList.contains('active')).toBe(false);
+
+  fireEvent.click(firstAccordionHeader);
+
+  setTimeout(() => {
+    expect(firstAccordionHeader.classList.contains('active')).toBe(true);
+
+    fireEvent.click(firstAccordionHeader);
+
+    setTimeout(() => {
+      expect(firstAccordionHeader.classList.contains('active')).toBe(false);
+
+      const state = store.getState();
+      const active = state.userReducer.active;
+      expect(active).toBe(null);
+    }, 0);
+  }, 0);
+
+  fireEvent.click(firstAccordionHeader);
+
+  setTimeout(() => {
+    const updatedState = store.getState();
+    const updatedActive = updatedState.userReducer.active;
+    expect(updatedActive).toBe(null);
+  }, 0);
 });
